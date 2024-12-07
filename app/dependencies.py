76e5111cb7d13fa5ea0840e6282e0b4d93c41eab
hm831 <kabunkinabc@gmail.com>
2024-12-07 15:33:17 +0900

    feat: 修改upload_img_server依赖的路径接收

--- app/dependencies.py
+++ app/dependencies.py
@@ -13,7 +13,7 @@ def upload_img_server_file(host: str, port: int, file: UploadFile):
 def upload_img_server(host: str, port: int, file_path: str):
     api_url = f"http://{host}:{port}/upload/"
     files = {
-        "file": open("/Users/yoru/Pictures/" + file_path, "rb")
+        "file": open(file_path, "rb")
     }
     response = requests.post(url=api_url, files=files).json()
     return response["url"]
